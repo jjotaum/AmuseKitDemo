40db7bcac05807072673ff8c6e15799ff6766a66
Jota Uribe <jjotaum@gmail.com>
2022-09-02 16:54:50 -0500

    Initial Commit

---
+++ Shared/AmuseKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmuseKitDemoApp.swift
+//  Shared
+//
+//  Created by Jota Uribe on 2/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AmuseKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
